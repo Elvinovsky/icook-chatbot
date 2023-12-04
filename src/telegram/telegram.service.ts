ee6c51106bdd49cfc47468fc61f0710d06d62d95
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-12-05 03:46:44 +0500

    update telegram on message

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,6 +1,16 @@
-import { Update } from 'nestjs-telegraf';
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
 import { Scenes, Telegraf } from 'telegraf';
 
 type Context = Scenes.SceneContext;
 @Update()
-export class TelegramService extends Telegraf<Context> {}
+export class TelegramService extends Telegraf<Context> {
+    @Start()
+    onStart(@Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<b> ПРивеТ, ${ctx.from.first_name}! </b>`).then((r) => r.text);
+    }
+
+    @On('text')
+    onMessage(@Message('text') message: string, @Ctx() ctx: Context) {
+        ctx.replyWithHTML(`<i> ${message} </i>`).then((r) => r.text);
+    }
+}
